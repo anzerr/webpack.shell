5641076e123a78e97a7b329cd2935e63aab7f329
anzerr <thoron174+anzerr@gmail.com>
2020-04-09 14:24:11 +0200

    🚧 added wait on kill

--- index.js
+++ index.js
@@ -37,15 +37,18 @@ class ShellPlugin {
 	}
 
 	clean() {
+		const wait = [];
 		for (let i in this.process) {
 			if (this.process[i]) {
-				kill(this.process[i].pid).catch(() => null).then(() => {
+				this.log('kill', this.process[i].pid);
+				wait.push(kill(this.process[i].pid).catch(() => null).then(() => {
 					safe(() => this.process[i].stdin.pause());
 					safe(() => this.process[i].kill());
-				});
+				}));
 			}
 			this.process[i] = null;
 		}
+		return Promise.all(wait);
 	}
 
 	log(...a) {
@@ -58,21 +61,27 @@ class ShellPlugin {
 		compiler.hooks.beforeCompile.tapAsync('MyPlugin', (params, callback) => {
 			this.log('beforeCompile', this.options.onStart);
 			if (this.options.onStart) {
-				this.clean();
-				this.process.push(run(this.options.onStart));
+				this.clean().then(() => {
+					this.process.push(run(this.options.onStart));
+					callback();
+				});
+			} else {
+				callback();
 			}
-			callback();
 		});
 		compiler.hooks.compile.tap('MyPlugin', (params) => {
-			console.log('compile');
+			this.log('compile');
 		});
 		compiler.hooks.afterEmit.tapAsync('MyPlugin', (params, callback) => {
-			this.log('afterCompile', this.options.onStart);
+			this.log('afterCompile', this.options.onEnd);
 			if (this.options.onEnd) {
-				this.clean();
-				this.process.push(run(this.options.onEnd));
+				this.clean().then(() => {
+					this.process.push(run(this.options.onEnd));
+					callback();
+				});
+			} else {
+				callback();
 			}
-			callback();
 		});
 	}
 
